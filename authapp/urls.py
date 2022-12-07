5f93f4ffdbdd20b020b7606f4b4237ef4ac3eafd
MyGitRep1 <uzinovev26@gmail.com>
2022-12-07 12:16:56 +0300

    lesson_6 News CRUD and permissions

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,17 +1,35 @@
 from django.urls import path
 
-from authapp import views
-from authapp.apps import AuthappConfig
+from mainapp import views
+from mainapp.apps import MainappConfig
 
-app_name = AuthappConfig.name
+app_name = MainappConfig.name
 
 urlpatterns = [
-    path("login/", views.CustomLoginView.as_view(), name="login"),
-    path("logout/", views.CustomLogoutView.as_view(), name="logout"),
-    path("register/", views.RegisterView.as_view(), name="register"),
+    path("", views.MainPageView.as_view(), name="main_page"),
+    path("news/", views.NewsListView.as_view(), name="news"),
+    path("news/create/", views.NewsCreateView.as_view(), name="news_create"),
     path(
-        "profile_edit/<int:pk>/",
-        views.ProfileEditView.as_view(),
-        name="profile_edit",
+        "news/<int:pk>/detail",
+        views.NewsDetailView.as_view(),
+        name="news_detail",
     ),
+    path(
+        "news/<int:pk>/update",
+        views.NewsUpdateView.as_view(),
+        name="news_update",
+    ),
+    path(
+        "news/<int:pk>/delete",
+        views.NewsDeleteView.as_view(),
+        name="news_delete",
+    ),
+    path("courses/", views.CoursesListView.as_view(), name="courses"),
+    path(
+        "courses/<int:pk>/",
+        views.CoursesDetailView.as_view(),
+        name="courses_detail",
+    ),
+    path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
+    path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
 ]
