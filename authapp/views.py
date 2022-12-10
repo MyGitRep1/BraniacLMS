7311a7cc8845f14a6efeecd91be0ce2d1944862c
MyGitRep1 <106673455+MyGitRep1@users.noreply.github.com>
2022-12-10 12:14:28 +0300

    Lesson 6 (#5)
    * lesson_6 Link to admin
    
    * lesson_6 Customizing admin panel
    
    * lesson_6 New user register form
    
    * lesson_6 User edit form
    
    * lesson_6 News CRUD and permissions
    
    * lesson_6 Feedbacks with AJAX

--- authapp/views.py
+++ authapp/views.py
@@ -1,104 +1,70 @@
-import os
-
-from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView, LogoutView
-from django.http.response import HttpResponseRedirect
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from django.utils.safestring import mark_safe
-from django.utils.translation import gettext_lazy as _
-from django.views.generic import TemplateView
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, TemplateView, UpdateView
 
-from authapp import models
+from mainapp import models as mainapp_models
 
 
-class CustomLoginView(LoginView):
-    def form_valid(self, form):
-        ret = super().form_valid(form)
-        message = _("Login success!<br>Hi, %(username)s") % {
-            "username": self.request.user.get_full_name()
-            if self.request.user.get_full_name()
-            else self.request.user.get_username()
-        }
-        messages.add_message(self.request, messages.INFO, mark_safe(message))
-        return ret
-
-    def form_invalid(self, form):
-        for _unused, msg in form.error_messages.items():
-            messages.add_message(
-                self.request,
-                messages.WARNING,
-                mark_safe(f"Something goes worng:<br>{msg}"),
-            )
-        return self.render_to_response(self.get_context_data(form=form))
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
 
 
-class CustomLogoutView(LogoutView):
-    def dispatch(self, request, *args, **kwargs):
-        messages.add_message(self.request, messages.INFO, _("See you later!"))
-        return super().dispatch(request, *args, **kwargs)
+class NewsListView(ListView):
+    model = mainapp_models.News
+    paginate_by = 5
+
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=False)
 
 
-class RegisterView(TemplateView):
-    template_name = "registration/register.html"
-
-    def post(self, request, *args, **kwargs):
-        try:
-            if all(
-                (
-                    request.POST.get("username"),
-                    request.POST.get("email"),
-                    request.POST.get("password1"),
-                    request.POST.get("password1") == request.POST.get("password2"),
-                )
-            ):
-                new_user = models.CustomUser.objects.create(
-                    username=request.POST.get("username"),
-                    first_name=request.POST.get("first_name"),
-                    last_name=request.POST.get("last_name"),
-                    age=request.POST.get("age") if request.POST.get("age") else 0,
-                    avatar=request.FILES.get("avatar"),
-                    email=request.POST.get("email"),
-                )
-                new_user.set_password(request.POST.get("password1"))
-                new_user.save()
-                messages.add_message(request, messages.INFO, _("Registration success!"))
-                return HttpResponseRedirect(reverse_lazy("authapp:login"))
-        except Exception as exp:
-            messages.add_message(
-                request,
-                messages.WARNING,
-                mark_safe(f"Something goes worng:<br>{exp}"),
-            )
-            return HttpResponseRedirect(reverse_lazy("authapp:register"))
+class NewsCreateView(PermissionRequiredMixin, CreateView):
+    model = mainapp_models.News
+    fields = "__all__"
+    success_url = reverse_lazy("mainapp:news")
+    permission_required = ("mainapp.add_news",)
 
 
-class ProfileEditView(LoginRequiredMixin, TemplateView):
-    template_name = "registration/profile_edit.html"
-    login_url = reverse_lazy("authapp:login")
+class NewsDetailView(DetailView):
+    model = mainapp_models.News
 
-    def post(self, request, *args, **kwargs):
-        try:
-            if request.POST.get("username"):
-                request.user.username = request.POST.get("username")
-            if request.POST.get("first_name"):
-                request.user.first_name = request.POST.get("first_name")
-            if request.POST.get("last_name"):
-                request.user.last_name = request.POST.get("last_name")
-            if request.POST.get("age"):
-                request.user.age = request.POST.get("age")
-            if request.POST.get("email"):
-                request.user.email = request.POST.get("email")
-            if request.FILES.get("avatar"):
-                if request.user.avatar and os.path.exists(request.user.avatar.path):
-                    os.remove(request.user.avatar.path)
-                request.user.avatar = request.FILES.get("avatar")
-            request.user.save()
-            messages.add_message(request, messages.INFO, _("Saved!"))
-        except Exception as exp:
-            messages.add_message(
-                request,
-                messages.WARNING,
-                mark_safe(f"Something goes worng:<br>{exp}"),
-            )
-        return HttpResponseRedirect(reverse_lazy("authapp:profile_edit"))
+
+class NewsUpdateView(PermissionRequiredMixin, UpdateView):
+    model = mainapp_models.News
+    fields = "__all__"
+    success_url = reverse_lazy("mainapp:news")
+    permission_required = ("mainapp.change_news",)
+
+
+class NewsDeleteView(PermissionRequiredMixin, DeleteView):
+    model = mainapp_models.News
+    success_url = reverse_lazy("mainapp:news")
+    permission_required = ("mainapp.delete_news",)
+
+
+class CoursesListView(TemplateView):
+    template_name = "mainapp/courses_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(CoursesListView, self).get_context_data(**kwargs)
+        context["objects"] = mainapp_models.Courses.objects.all()[:7]
+        return context
+
+
+class CoursesDetailView(TemplateView):
+    template_name = "mainapp/courses_detail.html"
+
+    def get_context_data(self, pk=None, **kwargs):
+        context = super(CoursesDetailView, self).get_context_data(**kwargs)
+        context["course_object"] = get_object_or_404(mainapp_models.Courses, pk=pk)
+        context["lessons"] = mainapp_models.Lesson.objects.filter(course=context["course_object"])
+        context["teachers"] = mainapp_models.CourseTeachers.objects.filter(course=context["course_object"])
+        return context
+
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
